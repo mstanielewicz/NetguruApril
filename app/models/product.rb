9fbb6f224516bec585beefbccefb31313fbeeb06
Mateusz Stanielewicz <mateusz.stanielewicz@gmail.com>
2015-04-18 13:50:10 +0200

    Task 6

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,10 +6,11 @@ class Product < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :description
   validates_presence_of :price
+  validates_format_of :price, :with => /\A[0-9]+([.][0-9]{1,2})\z/
 
   def average_rating
     if self.reviews.size > 0
-      self.reviews.average(:rating)
+      self.reviews.average(:rating).to_f
     else
       0
     end
